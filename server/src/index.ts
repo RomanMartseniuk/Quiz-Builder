28bcc047f2c769456e873d473d6b571e48459cc8
Roman Martseniuk <martseniuk.roma@gmail.com>
2026-07-01 14:43:15 +0300

    update server/package.json and server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import quizRouter from "./routes/quiz";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 app.use(express.json());
 
